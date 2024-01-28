6e321cda640109fe6a8a4e3c5d948c652d99fb9a
Falguni3005 <falgunirana40@gmail.com>
2024-01-28 14:23:35 +0530

    more functionality

--- ReactDay1/firstapp/src/components/TextForm.js
+++ ReactDay1/firstapp/src/components/TextForm.js
@@ -11,6 +11,11 @@ export default function TextForm(props)
   const handleOnChange = (event) => {
      setText(event.target.value);
   }
+  const handleTaOnClick = () =>
+  {
+    if(text===defaultvalue)
+    setText("");
+  }
   const handleReset = () => {
       setText(defaultvalue);
   }
@@ -20,14 +25,20 @@ export default function TextForm(props)
      setText(newText);
   }
   return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="9"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} onClick={handleTaOnClick} id="myBox" rows="6"></textarea>
         <button className="btnbtn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
         <button className="reset" onClick={handleReset}>Reset</button>
         <button className='btnbtn-primary' onClick={handleLpClick}>Convert to LowerCase</button>
         </div>
     </div>
+    <div className="container" my-3>
+      <h2>Text Summary</h2>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+    </div>
+    </>
   )
 }
